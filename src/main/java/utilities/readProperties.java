c4fb996b8d4435fdf261ef27d2512c1220fd59d3
TechnoStudy <info@techno.study>
2020-03-12 20:42:50 -0400

    recap

---
+++ src/main/java/utilities/readProperties.java
@@ -0,0 +1,16 @@
+package utilities;
+
+public class readProperties {
+
+        public static String getData(String whichData){
+
+            String path = "configuration.properteis";
+
+            String result = null;
+
+            
+
+        }
+
+
+}
